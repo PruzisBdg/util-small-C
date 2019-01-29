b91a2b266af1939430af15c79f7f9a3bc7527bb4
PruzisBdg <spjh1@wi.rr.com>
2019-01-28 22:03:56 -0600

    1. Now handles 'empty' corner cases. 2. For insertions, no uses Str_1stDelimiter() of the delimiters list.

--- src/wordlist/str_insert.c
+++ src/wordlist/str_insert.c
@@ -35,7 +35,7 @@ PRIVATE void makeGap( U8 *p, U8 gap)
 |
 |  Str_Insert()
 |
-|  Insert 'cnt' words from 'src' into 'dest', starting in front of word 'start'
+|  Insert  'cnt' words from 'src' into 'dest', starting in front of word 'start'
 |
 |  If 'start' exceeds the number of words in 'dest' then words from 'src' are
 |  appended to 'dest'.
@@ -43,30 +43,45 @@ PRIVATE void makeGap( U8 *p, U8 gap)
 |  If 'cnt' exceeds the number of words in 'src' then all words in 'src' are
 |  inserted into 'dest'.
 |
+|  If an insertion then a delimiter must be inserted after the end of the last
+|  inserted word (before the next word). If multiple delimiters are defined (in
+|  Str_Delimiters[]) then the 1st delimiter in that list is the one which is inserted.
+|
+|  Returns 'dest'.
+|
 ------------------------------------------------------------------------------*/
 
 PUBLIC U8 GENERIC * Str_Insert( U8 GENERIC *dest, U8 GENERIC const *src, U8 start, U8 cnt )
 {
-   U8 *p;
-   U8 bytesToInsert;
-
-   if( cnt > 0 )                                         // insert at least one word
+   if( cnt > 0 )                                                     // insert at least one word?
    {
-      p = (U8*)Str_GetNthWord(dest, start);              // Get the insertion point
-
-      /* Will insert from start of 'src' to end of nth word. (if start and finish are
+      /* Will insert from start of 'src' to end of nth word ('endOfInsert'). (if start and finish are
          the same letter then there's one byte to insert).
       */
-      bytesToInsert = (U8)(Str_GetEndWord((U8 GENERIC*)src, cnt-1) - src + 1); //
+      U8 *endOfInsert = Str_GetEndWord((U8 GENERIC*)src, cnt-1);     // Get end of cnt-th word in src.
 
-      /* Make a gap in 'dest' to fit the word to be inserted plus one space.
-      */
-      if(bytesToInsert > 0)
-      {
-         makeGap(p, bytesToInsert+1);
+      if(*endOfInsert != '\0')                                       // NOT end-of-string?...
+      {                                                              // ...meaning 'src' had at least word, so there's something to insert.
+         U8 bytesToInsert = (U8)(endOfInsert - src + 1);
 
-         strncpy((C8*)p, (C8 GENERIC *)src, bytesToInsert); // Copy it in the word to be inserted
-         p[bytesToInsert] = ' ';                            // and add the space after it
+         if(bytesToInsert > 0)                                       // Bytes to be inserted/appended?
+         {
+            U8 *p = (U8*)Str_GetNthWord(dest, start);                // Get the insertion point (in 'dest').
+
+            /* If insertion point is not end-of-string then make a gap in 'dest' to fit the
+               word to be inserted plus one space.
+            */
+            if(*p != '\0')                                           // Insertion point is NOT end-of-string? (i.e an append)
+            {
+               makeGap(p, bytesToInsert+1);                          // then make a gap for words to be inserted.
+               p[bytesToInsert] = Str_1stDelimiter();                // and add a delimiter after the last word.
+            }
+            else                                                     // else insertion point is end-of-string
+            {
+               p[bytesToInsert] = '\0';                              // so add string terminator to what we append (below).
+            }
+            memcpy(p, src, bytesToInsert);                           // Insert/append from 'src'.
+         }
       }
    }
    return dest;
