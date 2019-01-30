e9635561977b2e953ebc01b44fbebf2b9fc425a6
PruzisBdg <spjh1@wi.rr.com>
2019-01-29 20:48:04 -0600

    Passes all basic and corner cases. Now preserves addtional delimiters when splicing.

--- src/wordlist/str_delete.c
+++ src/wordlist/str_delete.c
@@ -12,15 +12,41 @@
 |
 |  Str_Delete()
 |
-|  Delete 'cnt' words from 'lst' from 'start' onwards.
+|  Delete 'cnt' words from 'lst' from 'start' onwards e.g
+|
+|     ("abc def ghi jkl", 1, 2) ->  "abc jkl"
+|
+|  Deletion preserves additional delimiters between words in this way
+|
+|     ("abc;def.ghi:..jkl", 1, 2) ->  "abc;..jkl"
+|
+|  i.e the ':' at the end of excised section gets dropped so that the splice
+|  preserves the number of delimiters.
 |
 ------------------------------------------------------------------------------*/
+
+// One past lead delimiter e.g  "...abc' -> '..abc', '..abc' -> '.abc', '.abc' -> 'abc'.
+PRIVATE U8 const * trimLeadDelimiter(U8 *wd)
+{
+   while( Str_Delimiter(*wd) ) { wd--; }     // Starting on a delimiter; backup until last char of previous word
+   return wd+2;                              // Forward 2 to skip earliest delimiter.
+}
+
+// e.g ("abc...def", 1) -> '..def' or ('abc.def', 1) -> 'def'
+PRIVATE U8 const *trimLeadof_NthWord(U8 *wd, U8 idx)
+{
+   wd = Str_GetNthWord(wd, idx);
+   return
+      idx == 0 || *wd == '\0'             // 1st word (idx == 0)? OR empty "" wd?
+         ? wd                             // then do NOT trim any leads; just return 'wd' as-is.
+         : trimLeadDelimiter(wd-1);       // else trim lead delimiter (above)
+}
 
 PUBLIC U8 GENERIC * Str_Delete( U8 GENERIC *lst, U8 start, U8 cnt )
 {
    // Note: We can case return from Str_GetNthWord() to non-const because we know that
    // return is 'lst' (which is non-const).
-   return strcpy( (C8*)Str_GetNthWord(lst, start), (C8 const *)Str_GetNthWord(lst, start+cnt) );
+   return strcpy( (C8*)Str_GetNthWord(lst, start), trimLeadof_NthWord(lst, start+cnt) );
 }
 
 // --------------------------- eof -------------------------------
