8092b42f5d78d006ed0e4a401c164dc2e0b37164
PruzisBdg <spjh1@wi.rr.com>
2019-02-04 22:10:21 -0600

    Adds 29 days for leap-year Feb.

--- src/timedate/legal_YMDHMS.c
+++ src/timedate/legal_YMDHMS.c
@@ -1,21 +1,24 @@
 /*---------------------------------------------------------------------------
 |
-|  
-|   
+|
+|
 |--------------------------------------------------------------------------*/
 
 #include "libs_support.h"
 #include "util.h"
 
-/* ------------------------------ daysInMonth --------------------------------- 
+/* ------------------------------ daysInMonth ---------------------------------
 
    Return days in 'month' of a non-leap year.
 */
 
 PRIVATE U8 const daysInMonthTbl[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
-PRIVATE U8 daysInMonth(U8 month) {
-   return daysInMonthTbl[month];
+PRIVATE U8 daysInMonth(U16 yr, U8 month) {
+   return
+      yr % 4 == 0 && month == 2        // Feb leap-year?
+         ? 29
+         : daysInMonthTbl[month];
 }
 
 /* ---------------------------- Legal_YMDHMS ------------------------------------
@@ -25,15 +28,15 @@ PRIVATE U8 daysInMonth(U8 month) {
 */
 PUBLIC BOOLEAN Legal_YMDHMS(S_DateTime const *t) {
 
-   #define _Years32Bit  (MAX_U32/366/24/3600)            // 32 bits can count 132years in seconds
+   #define _Years32Bit  (MAX_U32/366/24/3600)            // 32 bits can count 135years in seconds
 
    return
-   t->yr >= _2000AD && t->yr <= (_2000AD+_Years32Bit) && // Between 2000AD and 2130AD ? AND
-   t->mnth >= 1 && t->mnth <= 12 &&                      // Jan -> Dec?
-   t->day >= 1 && t->day <= daysInMonth(t->mnth) &&      // 1st -> 28(29)30,31st?
-   t->hr <= 23 &&                                        // hours 0-23?
-   t->min <= 59 &&                                       // 
-   t->sec <= 59;
+      t->yr >= _2000AD && t->yr <= (_2000AD+_Years32Bit) &&       // Between 2000AD and 2130AD ? AND
+      t->mnth >= 1 && t->mnth <= 12 &&                            // Jan -> Dec?
+      t->day >= 1 && t->day <= daysInMonth(t->yr, t->mnth) &&     // 1st -> 28(29)30,31st?
+      t->hr <= 23 &&                                              // hours 0-23?
+      t->min <= 59 &&                                             //
+      t->sec <= 59;
 }
 
 
