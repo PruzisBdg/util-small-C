e3788ce39e4e2a8f0aefa380691e5fc8dd7aebc7
PruzisBdg <SPruzina@badgermeter.com>
2019-02-13 11:11:54 -0600

    nothing

--- codeblocks_gcc/main.c
+++ codeblocks_gcc/main.c
@@ -9,36 +9,8 @@
 
 int main(void)
 {
-#if 0
-
-   S16 n;
-   C8 const inStr[] = "0";
-   ReadDirtyASCIIInt(inStr, &n);
-   printf("ReadDirtyASCIIInt \"%s\" -> %d\r\n", inStr, n);
-
-    T_IPAddrNum n = 0x55AA55AA;
-    C8 const str[] = "0.0.0.0";
-    //C8 const str[] = "0.1.2.3";
-    //C8 const str[] =  "16 .32. 48 . 64 ";
-    //C8 const str[] =  "just some text";
-    //C8 const str[]  = "7.89.22  1.2.3.4tail";
-
-    C8 const *p = ReadIPAddr(str, &n);
-    C8 b1[_MaxIPAddrChars+1];
-
-    printf("IP \"%s\" -> 0x%08lx -> %s, tail = \"%s\"\r\n", str, n, PrintIPAddr(b1, n), p == NULL ? "NULL" : (*p == '\0' ? "\\0" : p));
-   C8 b0[100];
-   strcpy(b0, "");
-    printf("ret = \"%s\"\r\n", Str_Insert(b0, "abc def", 3, 2));
-
-   C8 const *in = "abc";
-   C8 out[100];
-   printf("ToUpper \"%s\" -> \"%s\"\r\n", in, Word_1stToUpper(out, in));
-#endif
-
    OStream_Reset();
-   tiny1_printf("%+e", 0.0);
-//   tiny1_printf("%+12.2f", 1.2345E12);
+   tiny1_printf("%+012.2E", -12.3456);
    OStream_Print();
 
     return 0;
