b5bf740bcbede28bc706c9ef8a654ebf33b0ac8e
PruzisBdg <spjh1@wi.rr.com>
2019-04-13 09:31:35 -0500

    1. _NumArgs() now counts up to 64 args. 2. Adds to _Map1stRem2()

--- public/map_macros.h
+++ public/map_macros.h
@@ -25,11 +25,14 @@
 // (which are for the preprocessor). An empty list still counts as 1.
 #define _NumArgs(...) PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
 #define PP_NARG_(...) PP_ARG_N(__VA_ARGS__)
-#define PP_ARG_N(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,_17,_18,_19,_20,_21,_22,_23,_24,_25,_26,_27,_28,_29,_30,_31,_32,N,...) N
+#define PP_ARG_N(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,_17,_18,_19,_20,_21,_22,_23,_24,_25,_26,_27,_28,_29,_30,_31,_32,\
+                 _33,_34,_35,_36,_37,_38,_39,_40,_41,_42,_43,_44,_45,_46,_47,_48,_49,_50,_51,_52,_53,_54,_55,_56,_57,_58,_59,_60,N,...) N
 // It's that cheesy reverse-trick...
 // ...A reverse-list of numbers (below) get shoved rightwards past 'N' (above) by an arg-list which we insert in front (of the numbers).
 // e.g 11 args pushes the '11' under 'N'. That '11' is then returned by PP_ARG_N() (above) as the arg-count.
-#define PP_RSEQ_N() 32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0
+#define PP_RSEQ_N() \
+   60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,\
+   32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0
 
 /* -------------------------------------- Maps ----------------------------------------------- */
 
@@ -97,7 +100,8 @@
 #define _funcp2a14(_f, p0, a,b,c,d,e,f,g,h,i,j,k,l,m,n)  _f(p0,a,b), _f(p0,c,d), _f(p0,e,f), _f(p0,g,h), _f(p0,i,j), _f(p0,k,l),_f(p0,m,n)
 #define _funcp2a16(_f, p0, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p)  _f(p0,a,b), _f(p0,c,d), _f(p0,e,f), _f(p0,g,h), _f(p0,i,j), _f(p0,k,l),_f(p0,m,n),_f(p0,o,p)
 #define _funcp2a18(_f, p0, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r)  _f(p0,a,b), _f(p0,c,d), _f(p0,e,f), _f(p0,g,h), _f(p0,i,j), _f(p0,k,l),_f(p0,m,n),_f(p0,o,p),_f(p0,q,r)
-#define _funcp2a20(_f, p0, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t)  _f(p0,a,b), _f(p0,c,d), _f(p0,e,f), _f(p0,g,h), _f(p0,i,j), _f(p0,k,l),_f(p0,m,n),_f(p0,o,p),_f(p0,q,r)_f(p0,s,t)
+#define _funcp2a20(_f, p0, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t)  _f(p0,a,b), _f(p0,c,d), _f(p0,e,f), _f(p0,g,h), _f(p0,i,j), _f(p0,k,l),_f(p0,m,n),_f(p0,o,p),_f(p0,q,r),_f(p0,s,t)
+#define _funcp2a22(_f, p0, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v)  _f(p0,a,b), _f(p0,c,d), _f(p0,e,f), _f(p0,g,h), _f(p0,i,j), _f(p0,k,l),_f(p0,m,n),_f(p0,o,p),_f(p0,q,r),_f(p0,s,t),_f(p0,u,v)
 
 
 // e.g _Infix(+, a,b,c,d)  -> _infix4(+, a,b,c,d) -> a+b+c+d
