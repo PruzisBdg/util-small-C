2a6342e28951b9beafa8f7b0f7b1d212cafdb2e1
PruzisBdg <SPruzina@badgermeter.com>
2019-05-20 16:36:10 -0500

    Bracketed #ifndef SETB ... #endif in bit_field.h. Was duplicated in othe rfiles in WMBus project.

--- public/bit_field.h
+++ public/bit_field.h
@@ -3,6 +3,7 @@
 
 /* ---------------------------- Bits / Flags ------------------------------------ */
 
+	#ifndef SETB
 /* To manipulate a bit at a given (bit) position */
 #define _BitM(bit) (1 << (bit))
 #define _notB(bit)  0
@@ -10,9 +11,10 @@
 /*   To manipulate bits with a bit mask.  */
 #define SETB(reg,mask) {(reg) |= (mask);}
 #define CLRB(reg,mask) {(reg) &= ~(mask);}
-
+	
 #define BSET(reg,mask) ((reg) & (mask))
 #define BCLR(reg,mask) (!((reg) & (mask)))
+	#endif
 
 #define ALL_SET(v,m) (((v)&(m))==(m))
 #define ANY_SET(v,m) (((v)&(m))!=0)
