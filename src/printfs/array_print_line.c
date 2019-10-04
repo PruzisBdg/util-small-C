9034698ca5964e2039d0a426e6fc91bee1fed1ed
PruzisBdg <SPruzina@badgermeter.com>
2019-10-04 16:45:49 -0500

    Adds PrintU8sReversed_1Line()

--- src/printfs/array_print_line.c
+++ src/printfs/array_print_line.c
@@ -31,6 +31,31 @@ PUBLIC C8 const * PrintU8s_1Line(C8 *out, U16 outBufLen, C8 const *fmt, U8 const
 	return out;
 }
 
+/* ---------------------------------------------------------------------------------------------
+
+   Same as above but print bytes reversed (little-endian).
+*/
+PUBLIC C8 const * PrintU8sReversed_1Line(C8 *out, U16 outBufLen, C8 const *fmt, U8 const *src, size_t numBytes)
+{
+	// Print something if 'src' and 'out' exist and there's room for at least 'prefix'.
+	if(src != NULL && out != NULL) {
+      if(numBytes == 0) {                             // Nothing to print?
+         *out = '\0'; }                               // then output empty string.
+      else {
+         C8 *p = out;
+
+         U16 i; for(i = 0, src += (numBytes-1); i < numBytes; i++, src--) {
+            if(p - out + 10 > outBufLen)					// Look ahead. is there room to add another digit digit?
+               { break; }										// No! then add no more
+            p += sprintf(p, fmt, *src); }	   		   // else print just number.
+
+         // Bytes printed; back over any trailing separators.
+         while( isSeperator(*(--p)) ) { *p = '\0'; }}
+	   }
+	return out;
+}
+
+
 PUBLIC C8 const * PrintS16s_1Line(C8 *out, U16 outBufLen, C8 const *fmt, S16 const *src, size_t numBytes)
 {
 	// Print something if 'src' and 'out' exist and there's room for at least 'prefix'.
